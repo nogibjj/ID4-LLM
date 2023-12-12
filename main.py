3e874f80535a126fe4f419fa662e5ed8795b7d94
CR72322 <1209542541@qq.com>
2023-12-11 21:55:34 -0500

    update

--- main.py
+++ main.py
@@ -1,18 +1,33 @@
-"""
-Main cli or app entry point
-"""
+from flask import Flask, render_template, request
+import openai
+import os
 
-from mylib.calculator import add
-import click
+app = Flask(__name__)
 
+# Set your OpenAI API key from the environment variable
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
-@click.command("add")
-@click.argument("a", type=int)
-@click.argument("b", type=int)
-def add_cli(a, b):
-    click.echo(add(a, b))
+@app.route('/')
+def index():
+    return render_template('index.html')
 
+@app.route('/get_activity', methods=['POST'])
+def get_activity():
+    # Get the user's mood from the form
+    user_mood = request.form['mood']
 
-if __name__ == "__main__":
-    # pylint: disable=no-value-for-parameter
-    add_cli()
+    # Use OpenAI API to get an activity suggestion
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=f"I am feeling {user_mood}. What activity should I do?",
+        temperature=0.7,
+        max_tokens=64,
+        top_p=1
+    )
+
+    activity_suggestion = response.choices[0].text.strip()
+
+    return render_template('result.html', mood=user_mood, activity=activity_suggestion)
+
+if __name__ == '__main__':
+    app.run(port=8000)
